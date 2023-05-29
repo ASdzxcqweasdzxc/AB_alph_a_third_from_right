50f5c330454c62da3a99aa25d602cacd3c7a8a7e
alexkapralov <bopr2001@mail.ru>
2023-05-29 21:18:08 +0300

    corrected whole code from passing whole array to reading from input string with getchar() func

--- main.c
+++ main.c
@@ -1,169 +1,174 @@
 #include <stdio.h>
 
-int S0(const char c[], int i);
-int S1(const char c[], int i);
-int S2(const char c[], int i);
-int S3(const char c[], int i);
-int S4(const char c[], int i);
-int S5(const char c[], int i);
-int S6(const char c[], int i);
-int S7(const char c[], int i);
+int S1();
+int S2();
+int S3();
+int S4();
+int S5();
+int S6();
+int S7();
+
 int res = 0;
 
-int is_ab(const char c) {
-    if (c == 0) {
-        return 0;
-    } else if ((c != 'a') & (c != 'b')) {
-        return 1;
+int gc() {
+    int i  = getchar();
+    if (i == '\n' || i == EOF || i == 0) {
+        return 'e'; // means end of input
     }
-    return 2;
+    else if (i != 'a' && i != 'b') {
+        return 'w'; // wrong letter
+    }
+    return i; // a or b
 }
 
-int S1(const char c[], int i) { // bba
-    if (is_ab(c[i]) == 1) {
-        return (res = -1);
-    } else if (is_ab(c[i]) == 0) {
-        puts("bba");
-        return res = 'b';
-    } else if (c[i] == 'a') {
-        printf("bba -> baa, ");
-        S2(c, ++i);
-    } else {
-        printf("bba -> bab, ");
-        S3(c,++i);
-    }
-    return 0;
-}
-
-int S2(const char c[], int i) { //baa
-    if (is_ab(c[i]) == 1) {
-        return (res = -1);
-    } else if (is_ab(c[i]) == 0) {
-        puts("baa");
-        return res = 'b';
-    } else if (c[i] == 'a') {
-        printf("baa -> aaa, ");
-        S4(c,++i);
-    } else {
-        printf("baa -> aab, ");
-        S5(c,++i);
-    }
-    return 0;
-}
-
-int S3(const char c[], int i) { //bab
-    if (is_ab(c[i]) == 1) {
-        return (res = -1);
-    } else if (is_ab(c[i]) == 0) {
-        puts("bab");
-        return res = 'b';
-    } else if (c[i] == 'a') {
-        printf("bab -> aba, ");
-        S6(c,++i);
-    } else {
-        printf("bab -> abb, ");
-        S7(c,++i);
-    }
-    return 0;
-}
-
-int S4(const char c[], int i) { //aaa
-    if (is_ab(c[i]) == 1) {
-        return (res = -1);
-    } else if (is_ab(c[i]) == 0) {
-        puts("aaa");
-        return res = 'a';
-    } else if (c[i] == 'a') {
-        printf("aaa -> aaa, ");
-        S4(c,++i);
-    } else {
-        printf("aaa -> aab, ");
-        S5(c,++i);
-    }
-    return 0;
-}
-
-int S5(const char c[], int i) { //aab
-    if (is_ab(c[i]) == 1) {
-        return (res = -1);
-    } else if (is_ab(c[i]) == 0) {
-        puts("aab");
-        return res = 'a';
-    } else if (c[i] == 'a') {
-        printf("aab -> aba, ");
-        S6(c,++i);
-    } else {
-        printf("aab -> abb, ");
-        S7(c,++i);
-    }
-    return 0;
-}
-
-int S6(const char c[], int i) { //aba
-    if (is_ab(c[i]) == 1) {
-        return (res = -1);
-    } else if (is_ab(c[i]) == 0) {
-        puts("aba");
-        return res = 'a';
-    } else if (c[i] == 'a') {
-        printf("aba -> baa, ");
-        S2(c, ++i);
-    } else {
-        printf("aba -> bab, ");
-        S3(c, ++i);
-    }
-    return 0;
-}
-
-int S7(const char c[], int i) { //abb
-    if (is_ab(c[i]) == 1) {
-        return (res = -1);
-    } else if (is_ab(c[i]) == 0) {
-        puts("abb");
-        return res = 'a';
-    } else if (c[i] == 'a') {
-        printf("abb -> bba, ");
-        S1(c, i);
-    } else {
-        printf("abb -> bbb, ");
-        S0(c, ++i);
-    }
-    return 0;
-}
-
-int S0(const char c[], int i) { // 00b, 0bb, bbb
-    if (is_ab(c[i]) == 1){
-        return (res = -1);
-    } else if (is_ab(c[i]) == 0) {
+int S0() {
+    int ch = gc();
+    if (ch == 'e') {
         puts("bbb");
         return res = 'b';
-    } else if (c[i] == 'a') {
+    } else if (ch == 'w') {
+        return res = 'w';
+    } else if (ch == 'a') {
         printf("bbb -> bba, ");
-        S1(c,++i);
+        S1();
     } else {
         printf("bbb -> bbb, ");
-        S0(c,++i);
+        S0();
     }
-    return 0;
+    return '0';
+}
+
+int S1() {
+    int ch = gc();
+    if (ch == 'e') {
+        puts("bba");
+        return res = 'b';
+    } else if (ch == 'w') {
+        return res = 'w';
+    } else if (ch == 'a') {
+        printf("bba -> baa, ");
+        S2();
+    } else {
+        printf("bba -> bab, ");
+        S3();
+    }
+    return '1';
+}
+
+int S2() {
+    int ch = gc();
+    if (ch == 'e') {
+        puts("baa");
+        return res = 'b';
+    } else if (ch == 'w') {
+        return res = 'w';
+    } else if (ch == 'a') {
+        printf("baa -> aaa, ");
+        S4();
+    } else {
+        printf("baa -> aab, ");
+        S5();
+    }
+    return '2';
+}
+
+int S3() {
+    int ch = gc();
+    if (ch == 'e') {
+        puts("bab");
+        return res = 'b';
+    } else if (ch == 'w') {
+        return res = 'w';
+    } else if (ch == 'a') {
+        printf("bab -> aba, ");
+        S6();
+    } else {
+        printf("bab -> abb, ");
+        S7();
+    }
+    return '3';
+}
+
+int S4() {
+    int ch = gc();
+    if (ch == 'e') {
+        puts("aaa");
+        return res = 'a';
+    } else if (ch == 'w') {
+        return res = 'w';
+    } else if (ch == 'a') {
+        printf("aaa -> aaa, ");
+        S4();
+    } else {
+        printf("aaa -> aab, ");
+        S5();
+    }
+    return '4';
+}
+
+int S5() {
+    int ch = gc();
+    if (ch == 'e') {
+        puts("aab");
+        return res = 'a';
+    } else if (ch == 'w') {
+        return res = 'w';
+    } else if (ch == 'a') {
+        printf("aab -> aba, ");
+        S6();
+    } else {
+        printf("aab -> abb, ");
+        S7();
+    }
+    return '5';
+}
+
+int S6() {
+    int ch = gc();
+    if (ch == 'e') {
+        puts("aba");
+        return res = 'a';
+    } else if (ch == 'w') {
+        return res = 'w';
+    } else if (ch == 'a') {
+        printf("aba -> baa, ");
+        S2();
+    } else {
+        printf("aba -> bab, ");
+        S3();
+    }
+    return '6';
+}
+
+int S7() {
+    int ch = gc();
+    if (ch == 'e') {
+        puts("abb");
+        return res = 'a';
+    } else if (ch == 'w') {
+        return res = 'w';
+    } else if (ch == 'a') {
+        printf("abb -> bba, ");
+        S1();
+    } else {
+        printf("abb -> bbb, ");
+        S0();
+    }
+    return '7';
 }
 
 int main() {
-    char c[100];
-    int i = 0;
+//    int i;
     printf("Enter char sequence for {a,b}: ");
-    scanf("%s", c);
-    printf("Entered= %s\n", c);
+    int i = S0();
 
-    // start here
-//    printf("\nRes= %c\n", res); // debug
-    S0(c, i);
-//    printf("\nRes= %c\n", res); // debug
-    if (res == -1) {
-        puts("\nWrong alphabet!!!");
-    } else if (res == 'a') {
-        puts("\nNicely done =)");
+//    printf("\nFinal_res= %c \n", res);
+    if (res == 'a') {
+        puts("Well done!");
     } else if (res == 'b') {
-        puts("\nWrong word!!!");
+        puts("Wrong word!!!");
+    } else if (res == 'w') {
+        puts("Wrong alphabet!!!");
     }
 
     return 0;
